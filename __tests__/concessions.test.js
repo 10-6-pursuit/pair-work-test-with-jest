e33347afd564fe5d35f2220d2e119e1e473ba83c
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-06-22 11:35:02 -0400

    create: 8/8 tests for tickets.js and concessions.js files

--- __tests__/concessions.test.js
+++ __tests__/concessions.test.js
@@ -3,7 +3,7 @@ const  data  = require("../data/concessions");
 
 describe("getConcessionByID()", () => {
 
-    test("returns an object that match the input ID", () => {
+    test("should return an object that match the input ID", () => {
         expect(getConcessionByID(data, "g9sZdG1hI")).toEqual({ id: "g9sZdG1hI", name: "Small Popcorn", priceInCents: 929 })
     })
 
@@ -15,10 +15,19 @@ describe("getConcessionByID()", () => {
 
 describe("calculateTotalFromIDs()", () => {
 
-    const input = ["0Qs9Yp2NL", "KzWBehRAD"]
-    const actual = calculateTotalFromIDs(data, input);
-
     test("it should return a total of the sum from all prices of the provided IDs", () => {
+
+        const input = ["0Qs9Yp2NL", "KzWBehRAD"]
+        const actual = calculateTotalFromIDs(data, input);
+
         expect(actual).toEqual(1728);
     });
+    
+    test("if there's no matches with the provided ID returns 0 ", () => {
+
+        const input = ["0Qs9Yp2N", "KzWBehRA"]
+        const actual = calculateTotalFromIDs(data, input);
+
+        expect(actual).toEqual(0)
+    })
 })
--- __tests__/tickets.test.js
+++ __tests__/tickets.test.js
@@ -1,1 +1,41 @@
 const { calculateTotalFromTicketNames, getTicketByName } = require("../src/tickets");
+const data = require("../data/tickets");
+
+describe("getTicketByName()", () => {
+    
+
+    test("should return an object that match the input name", () => {
+        const actual = getTicketByName(data, "Senior Matinee");
+        const expected = {
+            id: "dlb8ka9N_", 
+            name: "Senior Matinee", 
+            priceInCents: 839 
+        }
+
+        expect(actual).toEqual(expected);
+    });
+
+    test("if there's no matches with the provided name returns 'null'", () => {
+
+        const actual = getTicketByName(data, "Adult VIP");
+
+        expect(actual).toEqual(null);
+    });
+
+
+});
+
+describe("calculateTotalFromTicketNames()", () => {
+
+    test("it should return a total of the sum from all prices of the provided name", () => {
+        const actual = calculateTotalFromTicketNames(data, ["Child Regular", "Adult Regular"]);
+
+        expect(actual).toEqual(2438);
+    });
+
+    test("if there's no matches with the provided name returns 0", () => {
+        const actual = calculateTotalFromTicketNames(data, ["Child VIP", "Senior VIP"])
+        
+        expect(actual).toEqual(0);
+    })
+});
